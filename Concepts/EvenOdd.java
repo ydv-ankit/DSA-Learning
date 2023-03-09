d159deb3139ac4dca636dca5b32d3ea617da17a1
Ankit Ydv <innocentankit1141@gmail.com>
2023-03-09 12:41:38 +0530

    added

---
+++ Concepts/EvenOdd.java
@@ -0,0 +1,10 @@
+// checks if number is even or not using bitwise operator
+public class EvenOdd {
+    public static void main(String[] args){
+        System.out.println(isEven(47));
+    }
+    private static boolean isEven(int num){
+        return (num & 1) != num;    // (num & 1) will give number itself if it is odd
+    }
+}
+
